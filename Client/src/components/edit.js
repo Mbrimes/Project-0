f44c7d035304475c30d5dba73f143a2d1b0a8eff
Marabs <marho.jak@gmail.com>
2023-02-17 15:00:03 +0100

    Working on this Project at 15:00:03 on 17/02/2023

--- Client/src/components/edit.js
+++ Client/src/components/edit.js
@@ -1,1 +1,38 @@
-edit
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router";
+
+export default function Edit() {
+	const [form, setForm] = useState({
+		name: "",
+		position: "",
+		level: "",
+		records: [],
+	});
+	const params = useParams();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		async function fetchData() {
+			const id = params.id.toString();
+			const response = await fetch(`http://localhost:5000/record/${params.id.toString()}`);
+
+			if (!response.ok) {
+				const message = `An error has occurred: ${response.statusText}`;
+				window.alert(message);
+				return;
+			}
+
+			const record = await response.json();
+			if (!record) {
+				window.alert(`Record with id ${id} not found`);
+				navigate("/");
+				return;
+			}
+
+			setForm(record);
+
+			return;
+		}, [params.id,navigate]
+	});
+
+}
