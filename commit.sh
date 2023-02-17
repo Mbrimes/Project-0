ee84fa166857180bed7e41e6004efdd4b6f7e9f8
Marabs <marho.jak@gmail.com>
2023-02-17 13:06:25 +0100

    Working on this Project0 at 13:06:25 on 17/02/2023

--- commit.sh
+++ commit.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-cd C:/Users/micha/Desktop/Codes/React Projects/Mern
+cd C:/Users/micha/Desktop/Codes/ReactProjects/Mern
 git add --all
 timestamp() {
   date +"at %H:%M:%S on %d/%m/%Y"
 }
-git commit -am "Working on this Project $(timestamp)"
+git commit -am "Working on this Project0 $(timestamp)"
 git push origin master
