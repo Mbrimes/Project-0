6b5edf99e224c26b2a9b0f030f253ab80e84888b
Marabs <marho.jak@gmail.com>
2023-02-17 12:08:12 +0100

    Working on it bruh at 12:08:12 on 17/02/2023

--- commit.sh
+++ commit.sh
@@ -4,5 +4,5 @@ git add --all
 timestamp() {
   date +"at %H:%M:%S on %d/%m/%Y"
 }
-git commit -am "Working on it $(timestamp)"
+git commit -am "Working on it bruh $(timestamp)"
 git push origin master
