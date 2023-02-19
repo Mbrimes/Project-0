82efeb7b30d9dfbb2479a1ea4f0fe26f8c30bc9b
Marabs <marho.jak@gmail.com>
2023-02-19 11:25:03 +0100

    Working on this Project at 11:25:03 on 19/02/2023

--- Server/index.js
+++ Server/index.js
@@ -1,1 +1,20 @@
 const {MongoClient} = require('mongodb');
+const Db = "mongodb+srv://Marabs:cMO5PuJHYQwwF8q9@cluster0.0h9y9gr.mongodb.net/employees?retryWrites=true&w=majority";
+const client = new MongoClient(Db);
+
+async function run() {
+	try{
+		await client.connect();
+		const db = client.db('employees');
+		const collection = db.collection('records');
+
+		//find the first document in the collection
+		const first = await collection.findOne();
+		console.log(first);
+	}
+	finally {
+			//close the database connection when finished or an error occurs
+			await client.close();
+		}
+}
+run().catch(console.error)
