b5a7656510fe28ec364eba21b585ebb087ee0994
Marabs <marho.jak@gmail.com>
2023-02-19 22:10:05 +0100

    Working on this Project at 22:10:05 on 19/02/2023

--- Server/Server.js
+++ Server/Server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
@@ -16,4 +16,4 @@ app.listen(port, () => {
  
   });
   console.log(`Server is running on port: ${port}`);
-});hg
+});
