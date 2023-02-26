6add88cb4b4d84f803fffc158c4dd5656a0c8d68
Marabs <marho.jak@gmail.com>
2023-02-26 20:25:03 +0100

    Working on this Project at 20:25:03 on 26/02/2023

--- Server/app.js
+++ Server/app.js
@@ -7,49 +7,12 @@ const cors = require('cors');
 // routes
 const books = require('./routes/api/books'); 
 
-const app = express();
-
-const db = require("./models");
-const Role = db.role;
+const app = express(); 
 
 // Connect Database
 connectDB();
 
-  function initial() {
-  Role.estimatedDocumentCount((err, count) => {
-    if (!err && count === 0) {
-      new Role({
-        name: "user"
-      }).save(err => {
-        if (err) {
-          console.log("error", err);
-        }
-
-        console.log("added 'user' to roles collection");
-      });
-
-      new Role({
-        name: "moderator"
-      }).save(err => {
-        if (err) {
-          console.log("error", err);
-        }
-
-        console.log("added 'moderator' to roles collection");
-      });
-
-      new Role({
-        name: "admin"
-      }).save(err => {
-        if (err) {
-          console.log("error", err);
-        }
-
-        console.log("added 'admin' to roles collection");
-      });
-    }
-  });
-};
+ 
 
 // cors
 app.use(cors({ origin: true, credentials: true }));
--- Server/config/db.js
+++ Server/config/db.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
+const dbs = require("./models");
+const Role = dbs.role;
+
 const connectDB = async () => {
   try {
     mongoose.set('strictQuery', true);
@@ -14,7 +17,44 @@ const connectDB = async () => {
   } catch (err) {
     console.error(err.message);
     process.exit(1);
-  } 
+  }; 
+
+
+   function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "moderator"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'moderator' to roles collection");
+      });
+
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+};
 };
 
 module.exports = connectDB;
