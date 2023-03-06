a675b497a07be77d97be10d100c98af07d6e574a
Marabs <marho.jak@gmail.com>
2023-03-06 17:40:04 +0100

    Working on this Project at 17:40:04 on 06/03/2023

--- Client/src/components/login.js
+++ Client/src/components/login.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Form from "react-validation/build/form";
 import Input from "react-validation/build/input";
 import CheckButton from "react-validation/build/button";
@@ -17,45 +17,31 @@ const required = value => {
   }
 };
 
-class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.handleLogin = this.handleLogin.bind(this);
-    this.onChangeUsername = this.onChangeUsername.bind(this);
-    this.onChangePassword = this.onChangePassword.bind(this);
+const Login = (props) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("");
 
-    this.state = {
-      username: "",
-      password: "",
-      loading: false,
-      message: ""
-    };
-  }
 
-  onChangeUsername(e) {
-    this.setState({
-      username: e.target.value
-    });
-  }
+  const onChangeUsername = e => {
+    setUsername(e.target.value)
+  };
 
-  onChangePassword(e) {
-    this.setState({
-      password: e.target.value
-    });
-  }
+  const onChangePassword = e => {
+    setPassword(e.target.value)
+  };
 
-  handleLogin(e) {
+  const handleLogin = e => {
     e.preventDefault();
 
-    this.setState({
-      message: "",
-      loading: true
-    });
+    setMessage({message: ""}),
+    setLoading({loading: true})
 
-    this.form.validateAll();
+    form =()=> validateAll();
 
-    if (this.checkBtn.context._errors.length === 0) {
-      AuthService.login(this.state.username, this.state.password).then(
+    if (this.checkBtn.context._errors.length === 0 ){
+      AuthService.login({username}, {password}).then(
         () => {
           this.props.router.navigate("/profile");
           window.location.reload();
@@ -68,19 +54,75 @@ class Login extends Component {
             error.message ||
             error.toString();
 
-          this.setState({
-            loading: false,
-            message: resMessage
-          });
+              setMessage({message: resMessage}),
+              setLoading({loading: false});
         }
-      );
-    } else {
-      this.setState({
-        loading: false
-      });
-    }
+        );
+    } else{setLoading({loading: false})}
   }
+  // constructor(props) {
+  //   super(props);
+  //   this.handleLogin = this.handleLogin.bind(this);
+  //   this.onChangeUsername = this.onChangeUsername.bind(this);
+  //   this.onChangePassword = this.onChangePassword.bind(this);
 
+  //   this.state = {
+  //     username: "",
+  //     password: "",
+  //     loading: false,
+  //     message: ""
+  //   };
+  // }
+
+  // onChangeUsername(e) {
+  //   this.setState({
+  //     username: e.target.value
+  //   });
+  // }
+
+  // onChangePassword(e) {
+  //   this.setState({
+  //     password: e.target.value
+  //   });
+  // }
+
+  // handleLogin(e) {
+  //   e.preventDefault();
+
+  //   this.setState({
+  //     message: "",
+  //     loading: true
+  //   });
+
+  //   this.form.validateAll();
+
+  //   if (this.checkBtn.context._errors.length === 0) {
+  //     AuthService.login(this.state.username, this.state.password).then(
+  //       () => {
+  //         this.props.router.navigate("/profile");
+  //         window.location.reload();
+  //       },
+  //       error => {
+  //         const resMessage =
+  //           (error.response &&
+  //             error.response.data &&
+  //             error.response.data.message) ||
+  //           error.message ||
+  //           error.toString();
+
+  //         this.setState({
+  //           loading: false,
+  //           message: resMessage
+  //         });
+  //       }
+  //     );
+  //   } else {
+  //     this.setState({
+  //       loading: false
+  //     });
+  //   }
+  // }
+ 
     return (
       <div className="col-md-12">
         <div className="card card-container">
@@ -91,9 +133,9 @@ class Login extends Component {
           />
 
           <Form
-            onSubmit={this.handleLogin}
+            onSubmit={handleLogin}
             ref={c => {
-              this.form = c;
+              {form} = c;
             }}
           >
             <div className="form-group">
@@ -102,8 +144,8 @@ class Login extends Component {
                 type="text"
                 className="form-control"
                 name="username"
-                value={this.state.username}
-                onChange={this.onChangeUsername}
+                value={username}
+                onChange={onChangeUsername}
                 validations={[required]}
               />
             </div>
@@ -114,8 +156,8 @@ class Login extends Component {
                 type="password"
                 className="form-control"
                 name="password"
-                value={this.state.password}
-                onChange={this.onChangePassword}
+                value={password}
+                onChange={onChangePassword}
                 validations={[required]}
               />
             </div>
@@ -123,19 +165,19 @@ class Login extends Component {
             <div className="form-group">
               <button
                 className="btn btn-primary btn-block"
-                disabled={this.state.loading}
+                disabled={loading}
               >
-                {this.state.loading && (
+                {loading && (
                   <span className="spinner-border spinner-border-sm"></span>
                 )}
                 <span>Login</span>
               </button>
             </div>
 
-            {this.state.message && (
+            {message && (
               <div className="form-group">
                 <div className="alert alert-danger" role="alert">
-                  {this.state.message}
+                  {message}
                 </div>
               </div>
             )}
