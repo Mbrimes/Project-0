f49057e271e439277259cca8bcb22cd9acfb6da9
Marabs <marho.jak@gmail.com>
2023-03-08 09:40:03 +0100

    Working on this Project at 09:40:03 on 08/03/2023

--- Server/app.js
+++ Server/app.js
@@ -6,10 +6,8 @@ const cors = require('cors');
 
 // routes
 const books = require('./routes/api/books'); 
-// const auth = require('./routes/auth.routes');
-// const user = require('./routes/user.routes');
-require('./routes/auth.routes')(app);
-require('./routes/user.routes')(app);
+const auth = require('./routes/auth.routes');
+const user = require('./routes/user.routes'); 
 
 const app = express();  
 
@@ -29,8 +27,8 @@ app.get('/', (req, res) => res.send('Hello world am done with MERN Stack!'));
 
 // use Routes
 app.use('/api/books', books);
-// app.use('/auth.routes', auth);
-// app.use('/user.routes', user);
+app.use('/auth.routes', auth);
+app.use('/user.routes', user);
 
 const port = process.env.PORT || 5000;
 
