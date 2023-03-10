de97a840af5c890875462cf5ee33764275f27736
Marabs <marho.jak@gmail.com>
2023-03-10 12:10:03 +0100

    Working on this Project at 12:10:03 on 10/03/2023

--- Client/src/components/Login/Home.js
+++ Client/src/components/Login/Home.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
 
 import UserService from "../../services/user.service";
 
 const Home = () => {
   const [content, setContent] = useState("");
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
 
   useEffect(() => {
     UserService.getPublicContent().then(
@@ -26,6 +28,33 @@ const Home = () => {
       <header className="jumbotron">
         <h3>{content}</h3>
       </header>
+      <div>
+          <div className="text-container">
+              <h1>
+                {user
+                  ? `Welcome,  ${user.result.name}`
+                  : 'Welcome to Blookify,'}
+              </h1>
+              <p>
+                Utilize over 7 million books and store the books you are
+                currently reading and the books that youve finished into your
+                own personal library.
+              </p>
+           
+              <div className="button-cont">
+                {user ? null : (
+                  <Link to="/login" className="btn-1 b-1">
+                    Login
+                  </Link>
+                )}
+
+                <Link to={user ? '/Addbook' : '/login'} className="btn-1 b-2">
+                  Get started
+                </Link>
+              </div>
+            
+          </div>
+    </div>
     </div>
   );
 };
