62a02bf4a34db3362c99b6958f6b92189dd52fd9
Marabs <marho.jak@gmail.com>
2023-03-10 10:20:03 +0100

    Working on this Project at 10:20:03 on 10/03/2023

--- Server/routes/user.routes.js
+++ Server/routes/user.routes.js
@@ -1,10 +1,8 @@
-const { authJwt } = require("../middleware");
+const { authJwt } = require("../middlewares");
 const controller = require("../controllers/user.controller");
-const express = require('express');
-const router = express.Router();
 
-module.exports = function(router) {
-  router.use(function(req, res, next) {
+module.exports = function(app) {
+  app.use(function(req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept"
@@ -12,8 +10,19 @@ module.exports = function(router) {
     next();
   });
 
-  router.get("/api/test/all", controller.allAccess);
+  app.get("/api/test/all", controller.allAccess);
 
-  router.get("/api/test/user", [authJwt.verifyToken], controller.userBoard);
+  app.get("/api/test/user", [authJwt.verifyToken], controller.userBoard);
 
+  app.get(
+    "/api/test/mod",
+    [authJwt.verifyToken, authJwt.isModerator],
+    controller.moderatorBoard
+  );
+
+  app.get(
+    "/api/test/admin",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.adminBoard
+  );
 };
