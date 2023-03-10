bd773faa540dc23d566c2bc4084b032047f5ea0c
Marabs <marho.jak@gmail.com>
2023-03-10 10:35:03 +0100

    Working on this Project at 10:35:03 on 10/03/2023

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,26 +1,11 @@
 const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
+
 const dbs = require("../models");
 const Role = dbs.role; 
-
-const connectDB = async () => {
-  try {
-    mongoose.set('strictQuery', true);
-    await mongoose.connect(db, {
-      useNewUrlParser: true,
-    });
-
-    console.log('MongoDB is Connected...');
-    initial();
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  };  
-};
-
-
-function initial() {
+ 
+ function initial() {
   Role.estimatedDocumentCount((err, count) => {
     if (!err && count === 0) {
       new Role({
@@ -35,5 +20,20 @@ function initial() {
     }
   });
 };
+
+const connectDB = async () => {
+  try {
+    mongoose.set('strictQuery', true);
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+    });
+
+    console.log('MongoDB is Connected...');
+    initial();
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  };  
+}; 
 
 module.exports = connectDB;
