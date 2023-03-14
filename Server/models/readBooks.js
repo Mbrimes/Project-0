70e81c3672f50d44f50e3e5e1c4af8d558059b07
Marabs <marho.jak@gmail.com>
2023-03-14 13:55:03 +0100

    Working on this Project at 13:55:03 on 14/03/2023

---
+++ Server/models/readBooks.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const readBookSchema = new mongoose.Schema({
+	title: String,
+	author: String,
+	pageLength: String,
+	selectedFile: String,
+	description: String,
+	creator: String,
+	createdAt: {
+		type: Date,
+		default: new Date(),
+	}
+})
+
+module.exports = readBooks = mongoose.model('readBook', readBookSchema);
