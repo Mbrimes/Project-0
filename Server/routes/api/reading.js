68bd90bfbd917370fb286b5ff0d024fe9b668d45
Marabs <marho.jak@gmail.com>
2023-03-20 12:50:02 +0100

    Working on this Project!!! at 12:50:02 on 20/03/2023

--- Server/routes/api/reading.js
+++ Server/routes/api/reading.js
@@ -2,7 +2,7 @@ const controller = require('../../controllers/posts');
 const { authJwt } = require("../../middleware");
 
 module.exports = function(app){
-app.get('/api/test/red',  controller.readingPosts);
+app.get('/', [authJwt.verifyToken], controller.readingPosts);
 app.post('/', [authJwt.verifyToken], controller.createreadingPost);
 app.patch('/:id', [authJwt.verifyToken], controller.updatePost);
 app.delete('/:id', [authJwt.verifyToken], controller.deletePost);
